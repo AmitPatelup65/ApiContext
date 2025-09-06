c2550920d09d1efe68fddf392d5a7e63b3078a0e
AmitPatelup65 <patelboy330@gmail.com>
2025-09-06 23:46:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App() {
 const {data,add,sub}=useContext(countercontext)
   return (
     <div className='flex justify-center h-screen items-center'>
-     <button onClick={add} className='bg-blue-500 border-1 px-3 py-3 rounded-lg active:scale-90'> +</button>
+     <button onClick={add} className='bg-blue-500 border-3 px-3 py-3 rounded-lg active:scale-90'> +</button>
      <h1 className='mx-3'>{data}</h1>
-     <button onClick={sub} className='bg-red-500 border-1 px-4 py-3 rounded-lg active:scale-90'> -</button>
+     <button onClick={sub} className='bg-red-500 border-3 px-4 py-3 rounded-lg active:scale-90'> -</button>
     </div>
   )
 }
